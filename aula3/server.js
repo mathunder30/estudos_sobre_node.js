708915d034319ad1834972c7bb77ce2e492374d6
Mateus Lopes <136674252+mathunder30@users.noreply.github.com>
2025-02-06 11:54:11 -0300

    atualização nodemon aula3

---
+++ aula3/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+// crud -> create, read, uptade, delete
+//         post, get, put, delete
+
+app.get('/', (req, res) =>{
+    res.send(`<form action="/" method = "POST"> Nome: <input type = "text" name = "nome"> <button>Enviar</button></form> `);
+
+});
+
+app.post('/', (req, res) =>{
+    res.send('obrigado pelo formulario enviado')
+})
+
+app.get('/contato', (req, res) =>{
+    res.send("Obrigado por acessar nossos contatos")
+})
+
+app.listen(3000, ()=>{
+    console.log('Servidor com a porta 3000');
+    console.log('Acessar http://localhost:3000');
+
+
+})
